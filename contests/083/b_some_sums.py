b57b3c927bd4515e2f57a206a94ce67dc7aadbf6
t.kuriyama <koolii0909@gmail.com>
2022-12-29 21:02:03 +0000

    add contests 083 B

---
+++ contests/083/b_some_sums.py
@@ -0,0 +1,15 @@
+N, A, B = map(int, input().split())
+sum = 0
+
+for i in range(1, N + 1):
+    digit = 0
+    num = i
+
+    while i > 0:
+        digit += i % 10
+        i //= 10
+
+    if A <= digit <= B:
+        sum += num
+
+print(sum)
