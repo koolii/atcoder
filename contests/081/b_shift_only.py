c1c525c12ce2864ba4ca57ece2cde3e990b93b6a
t.kuriyama <koolii0909@gmail.com>
2023-01-01 19:00:00 +0900

    add 081 B

---
+++ contests/081/b_shift_only.py
@@ -0,0 +1,21 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+ok = True
+count = 0
+
+while True:
+  for i in range(N):
+    # if A[i] % (2 + (count + 1)) == 1: はTLEが起きた
+    if A[i] % 2 == 1:
+      ok = False
+
+  if not ok:
+    break
+
+  for i in range(N):
+    A[i] //= 2
+
+  count += 1
+
+print(count)
