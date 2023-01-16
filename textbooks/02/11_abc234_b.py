728fd3e6bf5d8ccbce1b5e012cfac6e0efb1ab51
t.kuriyama <koolii0909@gmail.com>
2023-01-17 03:12:59 +0900

    feat: done textbook02 problem 11

---
+++ textbooks/02/11_abc234_b.py
@@ -0,0 +1,18 @@
+# link: https://atcoder.jp/contests/abc234/tasks/abc234_b
+# problem: Longest Segment
+
+from math import sqrt
+
+N = int(input())
+points = [list(map(int, input().split())) for i in range(N)]
+ans = 0
+
+for i in range(N-1):
+  for j in range(1, N-i):
+    dx = abs(points[i][0] - points[i+j][0])
+    dy = abs(points[i][1] - points[i+j][1])
+    # print(points[i], points[i + j], dx, dy, sqrt(dx + dy))
+    ans = max(ans, sqrt(dx ** 2 + dy ** 2))
+
+print(ans)
+
